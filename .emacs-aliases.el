f3c1beb59b12b133e3514be2767b7cd95dfd3daa
Danil Kutkevich <danil@kutkevich.org>
2012-02-29 13:43:01 +0400

    Conflicts merge leela-gentoo master to frink-gentoo

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -465,9 +465,9 @@
 (setq auto-mode-alist
       (cons '("/\\.curlrc\\'" . conf-mode) auto-mode-alist))
 (setq auto-mode-alist
-      (cons '("/\\conkyrc_calendar\\'" . conf-mode) auto-mode-alist))
+      (cons '("/conkyrc_calendar\\'" . conf-mode) auto-mode-alist))
 (setq auto-mode-alist
-      (cons '("/\\conkyrc_top\\'" . conf-mode) auto-mode-alist))
+      (cons '("/conkyrc_top\\'" . conf-mode) auto-mode-alist))
 
 ;;; Lisp.
 (setq auto-mode-alist
