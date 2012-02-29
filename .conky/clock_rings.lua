13c0699046dbc3e74a7abaa24448dcdff0110c5c
Danil Kutkevich <danil@kutkevich.org>
2012-02-29 10:40:14 +0400

    conky

--- .conky/clock_rings.lua
+++ .conky/clock_rings.lua
@@ -100,7 +100,7 @@ function conky_clock_rings()
     {
       name='loadavg',
       arg='1',
-      max=2,
+      max=5,
       bg_colour=0xffffff,
       bg_alpha=0.2,
       fg_colour=0xFFFFFF,
@@ -114,7 +114,7 @@ function conky_clock_rings()
     {
       name='loadavg',
       arg='2',
-      max=1.5,
+      max=2,
       bg_colour=0xffffff,
       bg_alpha=0.2,
       fg_colour=0xffffff,
