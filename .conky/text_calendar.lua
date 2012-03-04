8dfb225f3f5372e39fa82d6c07b90997dcca0a1c
Danil Kutkevich <danil@kutkevich.org>
2012-03-04 13:42:34 +0400

    conky

--- .conky/text_calendar.lua
+++ .conky/text_calendar.lua
@@ -25,41 +25,29 @@ Month given relative to the current:
 Leave blank for current month
 ]]
 
-function conky_text_calendar(arg)
+function conky_text_calendar()
   local day_per_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
-  local wdays = {'mo', 'tu', 'we', 'th', 'fr', '${color1}sa', '${color1}su'}
-  --    local wdays = {'пн', 'вт', 'ср', 'чт', 'пт', '${color1}сб', '${color1}вс${color}'}
   local dtable = os.date("*t")
   local year = dtable.year
   local month = dtable.month
   local day = dtable.day
-
-  local arg = tonumber(arg)
-  if arg == nil then arg = 0
-  else day = -1
-  end
 
   if month == 2 then
     if (year % 4 == 0) and (year % 100 ~= 0) or (year % 400 == 0) then
       day_per_month[2] = day_per_month[2]+1
     end
   end
-  month = month + arg
   month = month - math.floor(month / 12 ) * 12
   year = year + math.floor(month / 12 )
 
-  local sday = fWeekDayByDate(1, month, year)
-
-  local start_pos = ''
-
-  local result = start_pos .. '${color}'
+  local result = '${color}'
   do
-    local wday = sday
+    local wday = get_day_of_week(1, month, year)
     result = result .. '${font Liberation Sans:Regular:size=7}'
     result = result .. 'Mon  Tue  Wed  Thu  Fri  ${color1}Sat  Sun${color}'
     --		result = result .. 'пн вт ср чт пт ${color1}сб вс${color}'
     result = result .. '\n${font DejaVu Sans Mono:Regular:size=10}'
-    result = result .. start_pos .. string.rep('   ', wday - 1)
+    result = result .. string.rep('   ', wday - 1)
     local week = 1
     for i=1, day_per_month[month] do
       if i<10 then result = result .. ' ' end
@@ -70,7 +58,7 @@ function conky_text_calendar(arg)
       wday = wday + 1
       if wday >= 7 then result = result .. '${color}' end
       if wday == 8 then
-        result = result .. '\n' .. start_pos
+        result = result .. '\n'
         wday = 1
         week = week + 1
       elseif i < day_per_month[month] then
@@ -85,13 +73,11 @@ function conky_text_calendar(arg)
 
 end
 
---[[ day of the week based on date by ramZport ]]--
-function fWeekDayByDate(nDay, nMonth, nYear)
-  tWeekDay = {5, 6, 7, 1, 2, 3, 4}
-  local nAux=math.floor ((14-nMonth)/12)
-  nYear=nYear + 1 - nAux
-  nMonth=nMonth+12*nAux-2
-  local nIndex = math.fmod(7000+(nDay+nYear+math.floor (nYear/4)-math.floor (nYear/100)+math.floor (nYear/400)+math.floor ((31*nMonth)/12)), 7) + 1
-  return tWeekDay[nIndex]
+-- Returns the day of week integer. Compatible with Lua 5.0 and 5.1.
+-- <http://lua-users.org/wiki/DayOfWeekAndDaysInMonthExample>.
+function get_day_of_week(dd, mm, yy)
+  dw=os.date('*t',os.time{year=yy,month=mm,day=dd})['wday']
+  -- Monday is 1, Sunday is 7.
+  if dw == 1 then dw = 7 else dw = dw - 1 end
+  return dw
 end
-
