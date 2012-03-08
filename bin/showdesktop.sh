c767cce9c75105bcd46119f7e33ced48d42209ea
Danil Kutkevich <danil@kutkevich.org>
2012-03-08 13:47:54 +0400

    showdesktop.sh

--- bin/showdesktop.sh
+++ bin/showdesktop.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 # X Window Manager show desktop
+# <http://planetblur.org/hosted/awnforum/index.php?shard=forum&action=g_reply&ID=443>,
 # <http://ubuntuforums.org/showthread.php?t=1615199#td_post_10083165>.
-if wmctrl -m | grep "mode: ON"; then
+if wmctrl -m | grep -q '"showing the desktop" mode: ON'; then
     exec wmctrl -k off
 else
     exec wmctrl -k on
