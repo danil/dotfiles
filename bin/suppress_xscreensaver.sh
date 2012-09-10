0e2d8a6255ceee81e4dd4f87e5973395bda3d373
Danil Kutkevich <danil@kutkevich.org>
2012-09-10 19:06:57 +0400

    xscreensaver and electricsheep

---
+++ bin/suppress_xscreensaver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# devans 2008-07-19
+# This script will check to see if electricsheep is running, and if so, it will do nothing
+# If it is not running, it will invoke xscreensave-command -deactivate to prevent xscreensaver
+# from running.  Mostly this is useful is set as the command to call via heartbeat-cmd in
+# ~/.mplayer/config
+# <http://fugutabetai.com/?postid=292>.
+
+if [[ -z $(ps -ef | grep electricsheep | grep -v grep) ]] ; then
+    xscreensaver-command -deactivate
+fi
