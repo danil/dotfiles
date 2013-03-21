52916edabfc2f16d8afe3ec4614c9b74d9e7c0d6
Danil Kutkevich <danil@kutkevich.org>
2013-03-21 16:31:13 +0400

    emacs one more

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -499,6 +499,9 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 ;(setq ibuffer-use-other-window 1)
+;; <http://stackoverflow.com/questions/7598433/how-to-remove-a-key-from-a-minor-mode-keymap-in-emacs#7598754>.
+(define-key ibuffer-mode-map "\M-n" nil) ;unset ibuffer-forward-filter-group
+(define-key ibuffer-mode-map "\M-p" nil) ;unset ibuffer-backward-filter-group
 
 ;;; Interactively do things.
 ;; (ido-mode t)
