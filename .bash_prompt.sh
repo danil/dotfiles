26aa2461ea7ec42ddd06f115285bce89b07389ac
Danil Kutkevich <danil@kutkevich.org>
2013-10-19 02:53:46 +0400

    bash: prompt line (arror)

--- .bash_prompt.sh
+++ .bash_prompt.sh
@@ -39,7 +39,10 @@ function my_prompt_command {
     else
         ps1_exit_code=" ${ps1_red}error:$exit_code$ps1_plain"
     fi
-    PS1="${ps1_user}${ps1_dir}${ps1_exit_code}$(ps1_load)$(ps1_jobs)"'$(__git_ps1 " (%s)")\n'"${ps1_blue}\$${ps1_plain} "
+    # Prompt unicode arrow
+    # <http://crunchbang.org/forums/viewtopic.php?pid=127747#p127747>,
+    # <https://wiki.archlinux.org/index.php/Color_Bash_Prompt#From_Arch_Forum_.231>.
+    PS1="┌${ps1_user}${ps1_dir}${ps1_exit_code}$(ps1_load)$(ps1_jobs)"'$(__git_ps1 " (%s)")\n'"└${ps1_blue}\$${ps1_plain} "
 }
 PROMPT_COMMAND=my_prompt_command
 # Git prompt
