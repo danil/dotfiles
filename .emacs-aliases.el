de571d93dad306b8dc88abfb41305ed054ceb927
Danil Kutkevich <danil@kutkevich.org>
2013-10-25 21:55:14 +0400

    emacs highlight-symbol key binding

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -100,7 +100,26 @@
 (setq calendar-week-start-day 1)
 (global-font-lock-mode 1)
 
-(global-set-key (kbd "C-c d s") 'sort-lines)
+;;; Setting key with repeat
+;;; <http://stackoverflow.com/questions/7560094/two-key-shortcut-in-emacs-without-repressing-the-first-key#7560416>.
+(defmacro my-with-repeat-while-press-last-key (&rest body)
+  "Execute BODY and repeat while the user presses the last key."
+  (declare (indent 0))
+  `(let* ((repeat-key (and (> (length (this-single-command-keys)) 1)
+                           last-input-event))
+          (repeat-key-str (format-kbd-macro (vector repeat-key) nil)))
+     ,@body
+     (while repeat-key
+       (message "Type %s to repeat again" repeat-key-str)
+       (let ((event (read-event)))
+         (clear-this-command-keys t)
+         (if (equal event repeat-key)
+             (progn ,@body
+                    (setq last-input-event nil))
+           (setq repeat-key nil)
+           (push last-input-event unread-command-events))))))
+
+(global-set-key (kbd "C-c d o") 'sort-lines)
 
 ;;; BackspaceKey <http://emacswiki.org/BackspaceKey>.
 ;; (global-set-key [(control h)] 'delete-backward-char)
