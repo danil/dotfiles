3c1b82708acafebd0a1d3c84c3e6073d680c363e
Danil Kutkevich <danil@kutkevich.org>
2013-10-27 15:30:03 +0400

    compiz and xfce redundant key bindindigs removed, emacs set RAILS_TRUSTED_IP env variable

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -220,6 +220,7 @@
 ;;; Environment variables.
 (setenv "GIT_PAGER" "")
 ;; (setenv "ESHELL" (expand-file-name "/bin/zsh")) ;terminal <http://stackoverflow.com/questions/1568987/getting-emacs-to-respect-my-default-shell-options#1570246>
+(setenv "RAILS_TRUSTED_IP" "192.168.0.18") ;<https://github.com/charliesome/better_errors#security>.
 
 ;; ;;; <http://emacswiki.org/ScrollBar>.
 ;; (scroll-bar-mode -1)
