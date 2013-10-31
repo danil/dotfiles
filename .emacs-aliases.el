eae8b1ef6bae348d867035e0530d5d9d4f055189
Danil Kutkevich <danil@kutkevich.org>
2013-10-31 21:18:00 +0400

    emacs DISABLE_PRY_RAILS

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -230,6 +230,7 @@
 (setenv "GIT_PAGER" "")
 ;; (setenv "ESHELL" (expand-file-name "/bin/zsh")) ;terminal <http://stackoverflow.com/questions/1568987/getting-emacs-to-respect-my-default-shell-options#1570246>
 (setenv "RAILS_TRUSTED_IP" "192.168.0.18") ;<https://github.com/charliesome/better_errors#security>.
+(setenv "DISABLE_PRY_RAILS" "1")
 
 ;; ;;; <http://emacswiki.org/ScrollBar>.
 ;; (scroll-bar-mode -1)
