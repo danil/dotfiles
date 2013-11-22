c6757a125059b909827f0e3da09471b9456684ab
Danil Kutkevich <danil@kutkevich.org>
2013-11-22 12:28:11 +0400

    emacs: remove linum hook from magit-diff-mode

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -372,7 +372,6 @@
 (add-hook 'js-mode-hook 'my-linum-mode-hook)
 (add-hook 'lisp-mode-hook 'my-linum-mode-hook)
 (add-hook 'lua-mode-hook 'my-linum-mode-hook)
-(add-hook 'magit-diff-mode-hook 'my-linum-mode-hook)
 (add-hook 'makefile-gmake-mode-hook 'my-linum-mode-hook)
 (add-hook 'markdown-mode-hook 'my-linum-mode-hook)
 (add-hook 'nxml-mode-hook 'my-linum-mode-hook)
