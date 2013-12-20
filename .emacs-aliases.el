96b2acd9594d30f507c76112cbdd30f08c442ec0
Danil Kutkevich <danil@kutkevich.org>
2013-12-20 15:20:51 +0400

    emacs line truncation for ruby and more modes
    Line truncation for ruby, js, coffee and haml modes (truncate-lines).

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -23,6 +23,7 @@
 ;; (load-file (concat user-emacs-directory "my-recipes/my-color-theme.rcp"))
 (mapc 'load (directory-files
              (concat user-emacs-directory "my-recipes") t "^[^#].*rcp$"))
+
 ;;; el-get <http://github.com/dimitri/el-get>.
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;; (require 'el-get)
