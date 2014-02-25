3c866894de7798135d7ff5da6302631e2bc74f8b
Danil Kutkevich <danil@kutkevich.org>
2014-02-25 18:39:08 +0400

    emacs less css recipe; ssh: sotakone config

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -84,6 +84,7 @@
           "js-mode"
           "js2-mode" ;coffee mode defaults to js2-mode, which is not present in Emacs by default
           "less" ;do not remove, used for generic scroll!
+          "less-css-mode"
           "linum"
           "lisp-mode"
           "lua"
