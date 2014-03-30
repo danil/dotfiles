90389d61b167dc6ebfbbe159a71e4ff3a9c8d13d
Danil Kutkevich <danil@kutkevich.org>
2014-03-30 18:23:33 +0400

    emacs: disable smartparens new parens highlighting

--- .emacs.d/my-custom-variables.el
+++ .emacs.d/my-custom-variables.el
@@ -3,5 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-bar-width 7) ;scroll bar <http://emacswiki.org/emacs/ScrollBar>.
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
+ '(coffee-tab-width 2)
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(scroll-bar-width 7 t)) ;scroll bar <http://emacswiki.org/emacs/ScrollBar>
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sp-pair-overlay-face ((t nil))))
