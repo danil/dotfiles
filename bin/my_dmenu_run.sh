4eeb2da66dfbdd9755c745222d896465a030f194
Danil Kutkevich <danil@kutkevich.org>
2014-04-14 11:07:45 +0400

    undo my dmenu script

--- bin/my_dmenu_run.sh
+++ bin/my_dmenu_run.sh
@@ -7,17 +7,7 @@ foreground="white"
 background="black"
 selected_foreground="black"
 selected_background="OrangeRed1"
-dmenu_history=$HOME/.dmenu_history
 
-touch $dmenu_history
-(
-    IFS=:
-    # (tac $dmenu_history ; stest -flx $PATH | sort -u) \ # reverse order of lines <http://stackoverflow.com/questions/742466/how-can-i-reverse-the-order-of-lines-in-a-file#742485>
-    #                                                     # pipe multiple commands to a single command <http://stackoverflow.com/questions/11917708/pipe-multiple-commands-to-a-single-command#11917709>
-    #     | awk ' !x[$0]++'                               # removing duplicate lines without sorting <http://stackoverflow.com/questions/11532157/unix-removing-duplicate-lines-without-sorting#11532197>
-    (tac $dmenu_history ; stest -flx $PATH | sort -u) \
-        | awk ' !x[$0]++' \
-        | dmenu -fn $font -p $prompt \
-                -nf $foreground -nb $background \
-                -sf $selected_foreground -sb $selected_background $@
-) | tee --append "$dmenu_history" | ${SHELL:-"/bin/sh"} &
+dmenu_run -fn $font -p $prompt \
+    -nf $foreground -nb $background \
+    -sf $selected_foreground -sb $selected_background $@
