1b858b16dc93cb8fddaf9de761aa89f8728acf5d
Danil Kutkevich <danil@kutkevich.org>
2014-06-29 15:33:39 +0400

    awesome: theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/danil/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
