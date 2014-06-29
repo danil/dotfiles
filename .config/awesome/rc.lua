d06e4ceedc65212dc7cd2ee76af7d94510c7606e
Danil Kutkevich <danil@kutkevich.org>
2014-06-29 15:54:57 +0400

    awesome set xset rate

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -443,3 +443,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+os.execute("xset r rate 300 30")
