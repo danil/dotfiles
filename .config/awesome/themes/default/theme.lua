0383a1f3d5c9985ca8aca552a29cf37748df1d29
Danil Kutkevich <danil@kutkevich.org>
2014-07-05 23:25:08 +0400

    update awesome wallpaper

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
+theme.wallpaper = "~/local/share/backgrounds/gentoo-znurt-larry-abducted-1440x900.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
