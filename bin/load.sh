3e7f596ff5e25f7914022e652aad4b547c054b87
Danil Kutkevich <danil@kutkevich.org>
2014-07-07 00:53:51 +0400

    xchainkeys config for move window to workspace

--- bin/load.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
-# <http://en.wikipedia.org/wiki/Load_(computing)>
-echo "$(uptime| sed -e 's/.*load average: \([^,]*\),.*/\1/')"
