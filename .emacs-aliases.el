d9a6c618b110340bdad89c534e6c764e96b9e928
Danil Kutkevich <danil@kutkevich.org>
2014-10-25 19:10:26 +0000

    emacs: fix yasnippet mode recipe

--- .emacs-aliases.el
+++ .emacs-aliases.el
@@ -184,6 +184,7 @@
                   "yaml-mode"
                   "yascroll"
                   "yasnippet"
+                  "yasnippets"
                   ))
   (load-file (concat user-emacs-directory "my-recipes/" recipe ".rcp")))
 
