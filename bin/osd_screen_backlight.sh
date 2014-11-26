0dcd1be01456c4568b2b899a26b3be6b9d63176a
Danil Kutkevich <danil@kutkevich.org>
2014-11-27 00:01:54 +0300

    notify-send replace by dunstify

--- bin/osd_screen_backlight.sh
+++ bin/osd_screen_backlight.sh
@@ -2,4 +2,4 @@
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 
 # <https://wiki.archlinux.org/index.php/Desktop_notifications#Usage_in_programming>.
-notify-send "Screen backlight $(echo_screen_backlight.sh)%"
+dunstify --replace=1 "Screen backlight $(echo_screen_backlight.sh)%"
