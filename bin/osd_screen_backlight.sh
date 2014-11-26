5b05df1d41fd7614c424db19b2f95b3a899c0ac0
Danil Kutkevich <danil@kutkevich.org>
2014-11-26 09:11:25 +0300

    xchainkeys: screen backlight change notification

---
+++ bin/osd_screen_backlight.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+
+# <https://wiki.archlinux.org/index.php/Desktop_notifications#Usage_in_programming>.
+notify-send "Screen backlight $(echo_screen_backlight.sh)%"
