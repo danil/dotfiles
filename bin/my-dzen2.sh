2a463ee2afa5fa7e44a2c5e35f0798c129b4cc80
Danil Kutkevich <danil@kutkevich.org>
2014-12-19 12:55:56 +0300

    Replace lxqt-powermanagement and pnmixer.
    Replace lxqt-powermanagement and pnmixer by i3status.

---
+++ bin/my-dzen2.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+# <https://wiki.archlinux.org/index.php/I3#Shutdown.2C_reboot.2C_lock_screen>.
+dzen2 -x -302 -y 1 -tw 300 -ta r -fn 'Monospace:pixelsize=18:antialias=true:hinting=true:autohint=true:style=regular'
