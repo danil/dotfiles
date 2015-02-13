653696b7a712923f893a13a9b6c19421625ef489
Danil Kutkevich <danil@kutkevich.org>
2015-02-14 01:51:55 +0300

    dzen2: decrease font size

--- bin/my-dzen2.sh
+++ bin/my-dzen2.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 # <https://wiki.archlinux.org/index.php/I3#Shutdown.2C_reboot.2C_lock_screen>.
-dzen2 -x -420 -y 1 -tw 420 -ta r -fn 'Monospace:pixelsize=22:antialias=true:hinting=true:autohint=true:style=regular'
+dzen2 -x -395 -y 1 -tw 395 -ta r -fn 'Monospace:pixelsize=20:antialias=true:hinting=true:autohint=true:style=regular'
