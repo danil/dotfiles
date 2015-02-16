5ae3ee35cd8c1c54f9d5f137b5402fc24672af91
Danil Kutkevich <danil@kutkevich.org>
2015-02-16 21:40:47 +0300

    FIX: enable external monitor only if present

--- bin/xrandr-external.sh
+++ bin/xrandr-external.sh
@@ -2,4 +2,10 @@
 
 # Disable internal monitor and enable external monitor
 # <https://wiki.archlinux.org/index.php/xrandr#Example_1>.
-xrandr --output LVDS1 --off --output VGA1 --auto
+internal_display="LVDS1"
+external_display="VGA1"
+
+if (xrandr | grep "$external_display connected" >/dev/null 2>&1); then
+    xrandr --output $internal_display --off \
+           --output $external_display --auto
+fi
