96087013fe92b0b46fd8c935aee77b98853db1b8
Danil Kutkevich <danil@kutkevich.org>
2015-03-13 19:57:02 +0300

    FIX: internal monitor to external switcher

--- bin/my-xrandr-internal.sh
+++ bin/my-xrandr-internal.sh
@@ -2,5 +2,5 @@
 
 # Enable internal monitor and disable external monitor
 # <https://wiki.archlinux.org/index.php/xrandr#Example_1>.
-xrandr --output VGA1 --off --output LVDS1 --auto \
+xrandr --output VGA1 --off --output HDMI1 --off --output LVDS1 --auto \
     && ~/bin/my-keyboard-auto-repeat-rate.sh
