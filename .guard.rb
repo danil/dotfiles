81a2e02dfa4e17f877dc4f54d47a61e56a0565a6
Danil Kutkevich <danil@kutkevich.org>
2015-05-04 16:39:58 +0300

    NEW: ruby guard config

---
+++ .guard.rb
@@ -0,0 +1,16 @@
+# <http://stackoverflow.com/questions/17462475/guardfile-dsl-how-to-make-a-site-specific-config#17470276>,
+# <https://github.com/guard/guard/wiki/System-notifications>,
+# <http://www.rubydoc.info/github/guard/guard/Guard/Notifier/Emacs>.
+guard :rspec do
+  notification :tmux,
+               change_color: false,
+               color_location: 'status-left-bg', #to customize which tmux element will change color
+               failed: 'black',
+               pending: 'black',
+               success: 'black'
+  notification :emacs,
+               change_color: false,
+               failed: 'black',
+               success: 'black'
+  notification :notifysend
+end
