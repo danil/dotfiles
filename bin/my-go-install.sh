82b5fe61b779351c9464e411d78c485122d26df8
Danil Kutkevich <danil@kutkevich.org>
2015-05-10 19:55:42 +0300

    NEW: my installation script for go packages

---
+++ bin/my-go-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+my_install='go get -u'
+
+$my_install github.com/jgrahamc/httpdiff #diff http requests <https://github.com/jgrahamc/httpdiff>
+$my_install github.com/jingweno/ccat #colorizing cat <https://github.com/jingweno/ccat>
+$my_install github.com/laurent22/massren #rename multiple files using text editor <https://github.com/laurent22/massren>
+$my_install github.com/monochromegane/the_platinum_searcher #ag and ack replacement <https://github.com/monochromegane/the_platinum_searcher>
