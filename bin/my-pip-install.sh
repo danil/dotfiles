8ccfd8f2bb33a4f64b4cbc85699aab62bcf6146e
Danil Kutkevich <danil@kutkevich.org>
2015-05-10 18:12:35 +0300

    NEW: install all my pip packages

---
+++ bin/my-pip-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+my_install='pip install --user'
+
+$my_install glances #htop replacement <https://github.com/nicolargo/glances>
+$my_install httpie #curl replacement <http://httpie.org>
+$my_install mps-youtube #youtube player <https://github.com/np1/mps-youtube>
+$my_install ngxtop #real-time metrics for nginx server <https://github.com/lebinh/ngxtop>
+$my_install pythonpy #evaluate python expression like `py 2*2` <https://github.com/Russell91/pythonpy>
+$my_install stellar #db snapshot and restore <https://github.com/fastmonkeys/stellar>
+$my_install youtube-dl #Youtube downloader <https://github.com/rg3/youtube-dl>
