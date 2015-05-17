461632b69b87e9861e9a1aba533fabf53580ef20
Danil Kutkevich <danil@kutkevich.org>
2015-05-17 13:17:58 +0000

    my-git-mirror: add init.el.git

--- bin/my-git-mirror.sh
+++ bin/my-git-mirror.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 
-cd /var/git/dotfiles.git
-git push --mirror gitlab
-git push --mirror github
-git push --mirror bitbucket
+function my-mirror-for {
+    local name=${1}
+    cd /var/git/$name
+    git push --mirror gitlab
+    git push --mirror github
+    git push --mirror bitbucket
+}
+
+my-mirror-for "dotfiles.git"
+my-mirror-for "init.el.git"
