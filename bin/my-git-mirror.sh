7d26b58a236f3c5d47de0aa4877601ce921e9692
Danil Kutkevich <danil@kutkevich.org>
2015-05-31 18:18:49 +0000

    FIX: my git mirroring script annoying output

--- bin/my-git-mirror.sh
+++ bin/my-git-mirror.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 
+my_mirror_command="git push --quiet --mirror"
+
 function my-mirror-for {
     local name=${1}
     cd /var/git/$name
-    git push --mirror gitlab
-    git push --mirror github
-    git push --mirror bitbucket
+    $my_mirror_command gitlab
+    $my_mirror_command github
+    $my_mirror_command bitbucket
 }
 
 my-mirror-for "dotfiles.git"
