8be7a62c78daf09ff4c5380387e911fd38812d27
Danil Kutkevich <danil@kutkevich.org>
2015-06-06 16:47:07 +0000

    also mirror my repositories on gogs

--- bin/my-git-mirror.sh
+++ bin/my-git-mirror.sh
@@ -7,6 +7,7 @@ function my-mirror-for {
     local name=${1}
     cd /var/git/$name
     $my_mirror_command gitlab
+    $my_mirror_command gogs
     $my_mirror_command github
     $my_mirror_command bitbucket
 }
