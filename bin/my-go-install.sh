ca48b7e3d1455216306abb57fd5e984935d501cf
Danil Kutkevich <danil@kutkevich.org>
2015-06-20 13:29:35 +0000

    install emacs flycheck

--- bin/my-go-install.sh
+++ bin/my-go-install.sh
@@ -3,6 +3,7 @@
 my_install='go get -u'
 
 $my_install github.com/github/hub #github helper <https://github.com/github/hub>
+$my_install github.com/golang/lint/golint #used by emacs flycheck
 $my_install github.com/jgrahamc/httpdiff #diff http requests <https://github.com/jgrahamc/httpdiff>
 $my_install github.com/jingweno/ccat #colorizing cat <https://github.com/jingweno/ccat>
 $my_install github.com/laurent22/massren #rename multiple files using text editor <https://github.com/laurent22/massren>
