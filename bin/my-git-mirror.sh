1db994784f6912661563f9e592f0b6f7b80fbc54
Danil Kutkevich <danil@kutkevich.org>
2015-09-19 11:41:00 +0000

    git mirror h11 repos

--- bin/my-git-mirror.sh
+++ bin/my-git-mirror.sh
@@ -23,18 +23,18 @@ function my-mirror-for {
     done
 }
 
-my-mirror-for "dotfiles.git" "h10-gentoo-danil h2-gentoo-danil h3-arch-danil h3-gentoo-danil h4-sailfish-nemo h5-ubuntu-danil h6-gentoo-danil h7-ubuntu-medapp h9-gentoo-danil hl7rus-ubuntu-danil homer-gentoo-danil jobtest_molinos_ru lisa-gentoo-danil"
+my-mirror-for "dotfiles.git" "h10-gentoo-danil h11-gentoo-danil h2-gentoo-danil h3-arch-danil h3-gentoo-danil h4-sailfish-nemo h5-ubuntu-danil h6-gentoo-danil h7-ubuntu-medapp h9-gentoo-danil hl7rus-ubuntu-danil homer-gentoo-danil jobtest_molinos_ru lisa-gentoo-danil"
 my-mirror-for "el/ferm-mode.git" "master"
 my-mirror-for "el/ido-describe-bindings.git" "master"
 my-mirror-for "el/ido-occur.git" "master"
-my-mirror-for "el/init.el.git" "h10-gentoo-danil h2-gentoo-danil h3-gentoo-danil h5-ubuntu-danil h6-gentoo-danil h9-gentoo-danil"
-my-mirror-for "etc.git" "h10-gentoo h2-gentoo h3-arch h3-gentoo h4-sailfish h5-ubuntu h6-gentoo h9-gentoo homer-gentoo lisa-gentoo"
+my-mirror-for "el/init.el.git" "h10-gentoo-danil h11-gentoo-danil h2-gentoo-danil h3-gentoo-danil h5-ubuntu-danil h6-gentoo-danil h9-gentoo-danil"
+my-mirror-for "etc.git" "h10-gentoo h11-gentoo h2-gentoo h3-arch h3-gentoo h4-sailfish h5-ubuntu h6-gentoo h9-gentoo homer-gentoo lisa-gentoo"
 my-mirror-for "grubs.git" "h3-gentoo h6-gentoo h8-gentoo homer-gentoo"
 my-mirror-for "js/homepage.git" "master"
-my-mirror-for "kernels.git" "h10-gentoo h3-gentoo h6-gentoo h8-gentoo h9-gentoo"
+my-mirror-for "kernels.git" " h10-gentoo h11-gentoo h3-gentoo h6-gentoo h8-gentoo h9-gentoo"
 my-mirror-for "keys.git" "master"
 my-mirror-for "md/rc.git" "master"
 my-mirror-for "sh/overlays.git" "master net-analyzer/linode-longview sys-apps/mdp unsupported"
 my-mirror-for "sieves.git" "master"
-my-mirror-for "worlds.git" "h10 h2 h3 h6 h8 h9 homer"
+my-mirror-for "worlds.git" "h10 h11 h2 h3 h6 h8 h9 homer"
 my-mirror-for "zones.git" "master"
