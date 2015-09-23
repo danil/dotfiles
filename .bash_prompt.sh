d47dea1919cc45b826cddd11651333850c646279
Danil Kutkevich <danil@kutkevich.org>
2015-09-23 14:12:38 +0300

    simplify bash error alerts output

--- .bash_prompt.sh
+++ .bash_prompt.sh
@@ -76,13 +76,13 @@ function my_ps1_timer_show {
                 my_notify_urgency="low"
             else
                 my_notify_urgency="critical"
-                notify_title="exit:$my_exit_code $notify_title"
+                notify_title="☢ $my_exit_code $notify_title"
             fi
             dunstify --urgency=$my_notify_urgency \
                 "$notify_title" \
                 "$my_previous_command"
         fi
-        echo -n " time:$timer"
+        echo -n " ◷ $timer"
     fi
 }
 if [ -f ~/.git-prompt/contrib/completion/git-prompt.sh ]; then
@@ -100,7 +100,7 @@ function my_ps1_dynamic_variables {
     if [[ $my_exit_code -eq 0 || $my_exit_code -ge 128 ]]; then #set an error string for the prompt, if applicable (ignore kill e. g. 130 script terminated by control-c <http://www.tldp.org/LDP/abs/html/exitcodes.html>)
         ps1_exit_code=""
     else
-        ps1_exit_code=" error:$my_exit_code"
+        ps1_exit_code=" ☢ $my_exit_code" # ⚠ ☠ ☹ ☣
     fi
 
     ps1_load="$(ps1_load)"
