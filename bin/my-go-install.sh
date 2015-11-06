3bace4c0994d71f0d658f419e355aa2091336161
Danil Kutkevich <danil@kutkevich.org>
2015-11-06 21:34:45 +0300

    go: install goofys s3 file system mounter

--- bin/my-go-install.sh
+++ bin/my-go-install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-my_install='go get -u'
+my_install='go get -v -u'
 
 $my_install github.com/github/hub #github helper <https://github.com/github/hub>
 $my_install github.com/golang/lint/golint #used by emacs flycheck
 $my_install github.com/jgrahamc/httpdiff #diff http requests <https://github.com/jgrahamc/httpdiff>
 $my_install github.com/jingweno/ccat #colorizing cat <https://github.com/jingweno/ccat>
+$my_install github.com/kahing/goofys #mount s3 file system
 $my_install github.com/laurent22/massren #rename multiple files using text editor <https://github.com/laurent22/massren>
 $my_install github.com/lukasmartinelli/pgfutter #import csv and json into postgresql
 $my_install github.com/monochromegane/the_platinum_searcher #ag and ack replacement <https://github.com/monochromegane/the_platinum_searcher>
