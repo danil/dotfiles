4ce27e8c26ba377e28005b6c96aea563f18b5ae9
Danil Kutkevich <danil@kutkevich.org>
2015-11-27 23:13:16 +0300

    lein: update cider to unstable version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]}
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]} ;emacs cider want specific repl <https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md#replacing-default-repl-dependencies>
+ }
