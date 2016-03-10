ec01cfd89ba021d1dead88889185bcadca2afade
Danil Kutkevich <danil@kutkevich.org>
2016-03-10 23:51:59 +0300

    feat: lein: cider 0.11.0 version bump

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.2"]]}
+{:user {:plugins [[cider/cider-nrepl "0.11.0"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]} ;emacs cider want specific repl <https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md#replacing-default-repl-dependencies>
  }
