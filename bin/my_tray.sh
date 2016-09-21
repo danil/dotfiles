1a61ff5f522610b42d1d22aa2fde428439f5366a
Danil Kutkevich <danil@kutkevich.org>
2016-09-21 17:36:56 +0300

    align trayer-srg and i3status

---
+++ bin/my_tray.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+
+killall trayer-srg
+trayer-srg \
+    --edge top \
+    --align right \
+    --widthtype request \
+    --height 22 \
+    --distancefrom right --distance 490 \
+    --transparent true \
+    --alpha 0 \
+    --tint 000000 &
+killall xxkb
+xxkb &
