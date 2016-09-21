9d1e6d1afda94df2f86d0af752ec72623a90f92a
Danil Kutkevich <danil@kutkevich.org>
2016-09-22 01:07:37 +0300

    xautolock: remove notifier

--- bin/my_autolock.sh
+++ bin/my_autolock.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 killall xautolock 2> /dev/null
-xautolock -time 10 \
-          -notify 10 \
-          -notifier "notify-send --urgency=low 'Lock' 'in 10 seconds'" \
-          -locker ~/bin/my-lock.sh &
+xautolock -time 10 -locker ~/bin/my-lock.sh &
