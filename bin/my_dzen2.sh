d6e13c4aeeb727a73c7e6191cc91325db7224747
Danil Kutkevich <danil@kutkevich.org>
2016-09-22 21:10:07 +0300

    fix: dzen2 (i3status) respects dwm sys tray

--- bin/my_dzen2.sh
+++ bin/my_dzen2.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 # <https://wiki.archlinux.org/index.php/I3#Shutdown.2C_reboot.2C_lock_screen>.
-dzen2 -tw 495 -x -495 -y 0 \
+dzen2 -tw 495 -x -640 -y 0 \
       -h 27 -ta r \
       -fn 'Monospace:pixelsize=22:antialias=true:hinting=true:autohint=true:style=regular'
