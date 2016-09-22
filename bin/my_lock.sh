b4207894f07b33e60b599b3ed2c08b025b025732
Danil Kutkevich <danil@kutkevich.org>
2016-09-22 15:37:24 +0300

    i3lock: remove annoying message after lock

--- bin/my_lock.sh
+++ bin/my_lock.sh
@@ -3,3 +3,4 @@
 # <https://wiki.archlinux.org/index.php/I3#Shutdown.2C_reboot.2C_lock_screen>.
 # xset dpms force off && slock
 i3lock --ignore-empty-password --dpms --inactivity-timeout=10 --color=000000
+killall dunst &> /dev/null
