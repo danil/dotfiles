73c45fdf2d4f1d66ee3abcf1118f6d9aedc31374
Danil Kutkevich <danil@kutkevich.org>
2016-09-22 12:30:07 +0300

    fix: my xautolock script

--- bin/my_autolock.sh
+++ bin/my_autolock.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 killall xautolock &> /dev/null
-xautolock -time 10 -locker ~/bin/my-lock.sh &
+xautolock -time 10 \
+          -notify 10 \
+          -notifier "notify-send --urgency=low 'Lock' 'in 10 seconds'" \
+          -locker ~/bin/my_lock.sh &
