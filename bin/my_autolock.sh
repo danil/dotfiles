7504339b2c01e720146936fc26dd7e2d4aa52c9a
Danil Kutkevich <danil@kutkevich.org>
2016-09-30 17:33:55 +0300

    minor

--- bin/my_autolock.sh
+++ bin/my_autolock.sh
@@ -3,5 +3,5 @@
 killall xautolock &> /dev/null
 xautolock -time 10 \
           -notify 10 \
-          -notifier "notify-send --urgency=low 'Lock' 'in 10 seconds'" \
+          -notifier "notify-send --urgency=normal 'Lock' 'in 10 seconds'" \
           -locker ~/bin/my_lock.sh &
