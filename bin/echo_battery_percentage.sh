50015702ea0aba69d90a56b30b2b21fa371a4cec
Danil Kutkevich <danil@kutkevich.org>
2016-10-01 22:36:27 +0300

    add low battery level notification script

--- bin/echo_battery_percentage.sh
+++ bin/echo_battery_percentage.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 
-battery_level=`acpi --battery | grep -P -o '[0-9]+(?=%)'`
+acpi --battery | grep -P -o '[0-9]+(?=%)'
