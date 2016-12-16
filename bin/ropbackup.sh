f963e5f8dbed092ac6d957def94b325a5a7b057a
Danil Kutkevich <danil@kutkevich.org>
2016-12-16 11:11:48 +0300

    fix: backup respects logrotate name convention

--- bin/ropbackup.sh
+++ bin/ropbackup.sh
@@ -4,7 +4,7 @@ dir=${dir:-./}
 cmd="rsync --rsh 'ssh -p 22022' --checksum --human-readable --archive --partial --progress --stats"
 url="danil@staging.armor5games.com"
 dateprefix1=$(date --utc +%Y-%m-%d)
-dateprefix2=$(date --utc +%Y%m%dZ)
+dateprefix2=$(date --utc +%Y%m%d)
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -32,26 +32,26 @@ if ! command -v rsync >/dev/null 2>&1; then
 
 fi
 
-file=social_${dateprefix2}.zip
+file=social.zip-${dateprefix2}
 echo === $file ===
 eval "$cmd ${url}:/var/social_backups/${dateprefix1}-backup.zip $dir/$file"
 
-file=social_maindb_${dateprefix2}.zip
+file=social_maindb.zip-${dateprefix2}
 printf "\n"
 echo === $file ===
 eval "$cmd ${url}:/var/social_backups/${dateprefix1}-maindb.zip $dir/$file"
 
-file=flintvk_${dateprefix2}.zip
+file=flintvk.zip-${dateprefix2}
 printf "\n"
 echo === $file ===
 eval "$cmd ${url}:/var/flintvk_backups/${dateprefix1}-backup.zip $dir/$file"
 
-file=ropshard1_shard_${dateprefix2}.zip
+file=ropshard1_shard.zip-${dateprefix2}
 printf "\n"
 echo === $file ===
 eval "$cmd ${url}:/var/ropshard1_backups/${dateprefix1}-shard.zip $dir/$file"
 
-file=ropshard1_sharddb_${dateprefix2}.zip
+file=ropshard1_sharddb.zip-${dateprefix2}
 printf "\n"
 echo === $file ===
 eval "$cmd ${url}:/var/ropshard1_backups/${dateprefix1}-sharddb.zip $dir/$file"
