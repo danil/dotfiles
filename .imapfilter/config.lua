78cc3a71ecb738fc4f4e40a975fe612bf0f6b9b8
Danil Kutkevich <danil@kutkevich.org>
2018-05-02 09:53:26 +0300

    merge h10-gentoo-danil into h2-gentoo-danil

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -1,5 +1,7 @@
 package.path = package.path .. ";/home/danil/.imapfilter/?.lua"
 require "config/options"
 require "config/danil_at_kutkevich_org"
+-- require "config/lera_at_kutkevich_ru"
 
-danil_at_kutkevich_org_filtering()
+-- filtering_lera_at_kutkevich_ru()
+filtering_danil_at_kutkevich_org()
