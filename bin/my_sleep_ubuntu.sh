bce7c048cdcf26194efad13824ce9fd4bef69d65
Danil Kutkevich <danil@kutkevich.org>
2019-06-12 19:14:41 +0300

    fix: ubuntu xchainkeys keybingings

---
+++ bin/my_sleep_ubuntu.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+# <https://askubuntu.com/questions/1792/how-can-i-suspend-hibernate-from-command-line>
+systemctl suspend
---
+++ bin/my_windows_overview.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.overview.show();'
