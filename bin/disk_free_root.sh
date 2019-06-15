4d49b1c4ab8d47c3cf45c4d6996e739222e2d7b2
Danil Kutkevich <danil@kutkevich.org>
2019-06-15 14:41:43 +0300

    removing unused shell scripts

--- bin/disk_free_root.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
-
-df --human-readable --output=avail / \
-    | sed --quiet --expression='1!p' \
-    | sed --expression='s/[^0-9]*\([0-9.KMGT]*\).*/\1/'
