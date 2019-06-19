f102fe5e3f8ab17889dabb1b9a8a59b9d99454a9
Danil Kutkevich <danil@kutkevich.org>
2019-06-19 11:52:52 +0300

    renaming my sleep/lock scripts

--- bin/my_lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
-# <https://wiki.archlinux.org/index.php/I3#Shutdown.2C_reboot.2C_lock_screen>.
-# xset dpms force off && slock
-i3lock --ignore-empty-password --color=000000 || exit 1
-killall dunst &> /dev/null
-exit 0
