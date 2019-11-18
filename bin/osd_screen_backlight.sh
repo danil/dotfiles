8fe599b90811c0dd0e002164ae7b09fb39504240
Danil Kutkevich <danil@kutkevich.org>
2019-11-19 00:44:53 +0300

    refactor: osd screen backlight script

--- bin/osd_screen_backlight.sh
+++ bin/osd_screen_backlight.sh
@@ -6,6 +6,10 @@ source osd_progress_bar.sh
 percentage=$(echo_screen_backlight.sh)
 
 # ☼ <https://wiki.archlinux.org/index.php/Desktop_notifications#Usage_in_programming>.
-dunstify --replace=1 --urgency=low \
-         "☀ $(printf '%3s' ${percentage})%" \
-         "$(osd_progress_bar ${percentage})"
+if pgrep -x "xfce4-notifyd" > /dev/null
+then
+    killall xfce4-notifyd
+fi
+
+/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
+notify-send --urgency=low "☀ $(printf '%3s' ${percentage})%"
