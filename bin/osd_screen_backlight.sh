cc952bf0800f888066976351ca1568a30560b6c7
Danil Kutkevich <danil@kutkevich.org>
2019-11-20 21:13:09 +0300

    fix: replacing xfce4-notifyd by dunst notifications

--- bin/osd_screen_backlight.sh
+++ bin/osd_screen_backlight.sh
@@ -6,10 +6,6 @@ source osd_progress_bar.sh
 percentage=$(echo_screen_backlight.sh)
 
 # ☼ <https://wiki.archlinux.org/index.php/Desktop_notifications#Usage_in_programming>.
-if pgrep -x "xfce4-notifyd" > /dev/null
-then
-    killall xfce4-notifyd
-fi
-
-/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
-notify-send --urgency=low "☀ $(printf '%3s' ${percentage})%"
+dunstify --replace=1 --urgency=low \
+         "☀ $(printf '%3s' ${percentage})%" \
+         "$(osd_progress_bar ${percentage})"
