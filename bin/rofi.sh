42882ea3c408ff9e7855ba34badd185861bdb400
Danil Kutkevich <danil@kutkevich.org>
2024-07-18 12:12:38 +0300

    fixing theme script

---
+++ bin/rofi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+
+. binpath.sh
+
+/home/danil/.local/usr/local/bin/rofi -show combi -modi top -modes [run,drun,window,windowcd,combi,top,keys,filebrowser,recursivebrowser
