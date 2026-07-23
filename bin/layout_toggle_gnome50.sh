d255846df87af6853ad91afce6432d0be7f391f2
Danil Kutkevich <danil@kutkevich.org>
2026-07-23 09:23:55 +0300

    minor

--- bin/layout_toggle_gnome50.sh
+++ bin/layout_toggle_gnome50.sh
@@ -3,5 +3,5 @@
 
 set -e
 
-# Presses Super+Space down and releases them.
+# Presses Super+Space down and releases them <sup><sub>`sudo pkill ydotoold ; sudo ydotoold --socket-path="/run/user/1000/.ydotool_socket" --socket-own="1000:1000" &`</sub></sup>.
 ydotool key 125:1 57:1 57:0 125:0
