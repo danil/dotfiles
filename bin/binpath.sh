d178e90afafb339b7b81db784845c8b5336c4230
Danil Kutkevich <danil@kutkevich.org>
2026-08-17 08:27:20 +0000

    fixing gitmirror script

---
+++ bin/binpath.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# This file is part of Danil Kutkevich <danil@kutkevich.org> home.
+
+HOME=/home/danil
+HOMEBINDIR="$HOME"/bin
+LOCALBINDIR="$HOME"/.local/bin
+USRLOCALBINDIR="$HOME"/.local/usr/local/bin
+BREWBINDIR=/home/linuxbrew/.linuxbrew/bin
+CONFIGDIR="$HOME"/.config
+SHAREDIR="$HOME"/.local/share
+SNAPDIR="$HOME"/snap
+APPIMAGEDIR="$HOME"/Applications
+NIXBINDIR="$HOME"/.nix-profile/bin
--- bin/gitmirror_test.sh
+++ bin/gitmirror_test.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env sh
 # This file is part of Danil Kutkevich <danil@kutkevich.org> home.
 
-################################################################################
-################################################################################
-################################################################################
-# . /home/danil/bin/binpath.sh
-. /tmp/danil/dotfiles/bin/binpath.sh
+. /home/danil/bin/binpath.sh
 
 SCRIPT_NAME="$HOMEBINDIR"/gitmirror
 
