cc33e667e44d6b7ef2a7450e626983363201c534
Danil Kutkevich <danil@kutkevich.org>
2026-08-19 20:08:34 +0300

    refactoring shell script tests

--- bin/theme_test.sh
+++ bin/theme_test.sh
@@ -5,5 +5,14 @@
 
 SCRIPT_NAME="$HOMEBINDIR"/theme.sh
 
-dash -n "$SCRIPT_NAME"
-bash -n "$SCRIPT_NAME"
+dash -n        "$SCRIPT_NAME"
+bash -n        "$SCRIPT_NAME"
+bash-3.2.57 -n "$SCRIPT_NAME"
+bash-4.4.18 -n "$SCRIPT_NAME"
+sh -n          "$SCRIPT_NAME"
+
+dash        "$SCRIPT_NAME" --help
+bash        "$SCRIPT_NAME" --help
+bash-3.2.57 "$SCRIPT_NAME" --help
+bash-4.4.18 "$SCRIPT_NAME" --help
+sh          "$SCRIPT_NAME" --help
